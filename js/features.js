6820696b7997934d56ee5e09b35a6e5c4e88c644
bidduthosen <mdbidduthosen34@gmail.com>
2025-02-06 22:52:12 +0600

    donate complate than input field clear setup

--- js/features.js
+++ js/features.js
@@ -4,3 +4,20 @@ function setContainerActiveBtn (id){
     // remove class name
     document.getElementById(id).classList.remove('btn-outline')
 }
+
+// donate button even function
+
+function getDonationBalanceSetById (inputField, DonateId, myBalance){
+    const inputFiled = document.getElementById(inputField);
+    const inputValue = parseFloat(inputFiled.value);
+
+    let donateBalance = document.getElementById(DonateId);
+    let noakhaliDonateBalanceNum = parseFloat(donateBalance.innerText);
+    donateBalance.innerText = inputValue + noakhaliDonateBalanceNum;
+
+    let myCurrentBalance = document.getElementById(myBalance);
+    let myCurrentBalanceText = parseFloat(myCurrentBalance.innerText);
+    myCurrentBalance.innerText = myCurrentBalanceText - inputValue;
+
+    document.getElementById(inputField).value = '';
+}
--- js/utilites.js
+++ js/utilites.js
@@ -8,15 +8,20 @@ document.getElementById('history-contain-btn').addEventListener('click', functio
 });
 
 
-
+// noakhali donate handlar
 document.getElementById('flood-at-noakhali-btn').addEventListener('click', function(){
-    const noakhaliInputFiled = document.getElementById('flood-at-noakhali-input-filed');
-    const noakhaliInputValue = parseFloat(noakhaliInputFiled.value);
-    let noakhaliDonateBalance = document.getElementById('noakhali-donate-balance');
-    let noakhaliDonateBalanceNum = parseFloat(noakhaliDonateBalance.innerText);
-    noakhaliDonateBalance.innerText = noakhaliInputValue + noakhaliDonateBalanceNum;
+    getDonationBalanceSetById('flood-at-noakhali-input-filed', 'noakhali-donate-balance', 'my-balance')
+    
+});
 
-    let myCurrentBalance = document.getElementById('my-balance');
-    let myCurrentBalanceText = parseFloat(myCurrentBalance.innerText);
-    myCurrentBalance.innerText = myCurrentBalanceText - noakhaliInputValue;
+// feni donate button
+document.getElementById('feni-donate-btn').addEventListener('click', function(){
+    getDonationBalanceSetById('feni-input-field', 'feni-donate-balance', 'my-balance')
+    
 })
+// aid donate button
+document.getElementById('aid-donate-btn').addEventListener('click', function(){
+    getDonationBalanceSetById('aid-donate-input-field', 'aid-donate-balance', 'my-balance')
+    
+})
+
